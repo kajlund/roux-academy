951f706fa9f54cd1e4fdd2b08951e7a98274a20f
Kaj Lund <kaj.lund@gmail.com>
2020-08-20 21:29:37 +0300

    Adds error handling and view template

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const createError = require('http-errors');
 
 const logConfig = {
   level: 'error',
@@ -48,6 +49,22 @@ fastify.decorateReply('locals', { speakerNames: [] });
 fastify.addHook('preHandler', async function (req, reply) {
   reply.locals.speakerNames = await fastify.data.speakers.getNames();
   return;
+});
+
+fastify.setNotFoundHandler(function (req, reply) {
+  const error = createError(404, `The page "${req.url}" was not found`, { expose: true });
+  reply.statusCode = 404;
+  reply.view('error.ejs', { error });
+});
+
+fastify.setErrorHandler(function (error, req, reply) {
+  const status = error.statusCode || 500;
+  const message = status >= 500 ? 'Internal Server Error' : error.message;
+  const httpError = createError(status, message, { expose: false });
+
+  status < 500 ? reply.log.info(error) : reply.log.error(error);
+  reply.statusCode = status;
+  reply.view('/error.ejs', { error: httpError });
 });
 
 fastify.register(require('./routes'));
