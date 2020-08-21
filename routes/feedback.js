224637051a60d82723f96777fb9ec75713ac487f
Kaj Lund <kaj.lund@gmail.com>
2020-08-21 20:15:20 +0300

    Adds feedback template with dynamic feedback list

--- routes/feedback.js
+++ routes/feedback.js
@@ -1,7 +1,11 @@
 module.exports = async (fastify) => {
-  fastify.get('/', async function () {
+  fastify.get('/', async function (req, reply) {
     const feedback = await this.data.feedback.getList();
-    return feedback;
+    return reply.view('/layout/index.ejs', {
+      pageTitle: 'Feedback',
+      template: 'feedback',
+      feedback,
+    });
   });
 
   fastify.post('/', () => {
